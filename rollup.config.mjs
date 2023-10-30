dd5fe79243b80fdaaf4648f61b9dfa07c4ad1e9e
mitchellkim94 <mitchellkim94@icloud.com>
2023-10-30 18:45:28 +0900

    chore: project setup

---
+++ rollup.config.mjs
@@ -0,0 +1,39 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import swc, { defineRollupSwcOption } from "rollup-plugin-swc3";
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: './dist/bundle.cjs',
+            format: 'cjs',
+            sourcemap: 'inline'
+        },
+        {
+            file: './dist/bundle.mjs',
+            format: 'esm',
+            sourcemap: 'inline'
+        }
+    ],
+    plugins: [
+        PeerDepsExternalPlugin(),
+        nodeResolve({
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }),
+        commonjs(),
+        terser(),
+        typescript(),
+        swc(defineRollupSwcOption({
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }))
+    ],
+    external: [
+        'react',
+        'react-dom',
+        '@emotion/react',
+    ]
+}
