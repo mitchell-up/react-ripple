2f72925ac6568aee5f728bf5a7d3ac589cf9464b
mitchellkim94 <mitchellkim94@icloud.com>
2023-11-08 20:54:09 +0900

    chore: vanilla-extract adopted.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
 import swc, { defineRollupSwcOption } from "rollup-plugin-swc3";
+import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin'
 
 export default {
     input: 'src/index.ts',
@@ -20,6 +21,7 @@ export default {
         }
     ],
     plugins: [
+        vanillaExtractPlugin(),
         PeerDepsExternalPlugin(),
         nodeResolve({
             extensions: ['.js', '.jsx', '.ts', '.tsx'],
