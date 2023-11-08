78617918cc7733d614fb7c8ef2d463d0c3b25f3e
mitchellkim94 <mitchellkim94@icloud.com>
2023-11-08 21:35:16 +0900

    feat(example): Vite plugin added for vanilla-extract

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({jsxImportSource: "@emotion/react"})],
+  plugins: [react(), vanillaExtractPlugin()],
 })
