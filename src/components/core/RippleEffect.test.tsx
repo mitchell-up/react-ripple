f2333d2d904ec7618b3ea89ed1e93f64ac80d546
mitchellkim94 <mc.kim@rangex.co.kr>
2023-11-09 23:59:46 +0900

    test: testcase added

--- src/components/core/RippleEffect.test.tsx
+++ src/components/core/RippleEffect.test.tsx
@@ -1,5 +1,17 @@
 describe('Ripple', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(1 + 2).toBe(3);
-    });
+    test('if a component has a RippleEffect inside itself and it is clicked, the ripple effect will appear.', () => {
+
+    })
+
+    test('if a component is wrapped by RippleEffectContainer and clicked, the ripple effect will appear.', () => {
+
+    })
+
+    test('if ripple color set, ripple effect show that color.', () => {
+
+    })
+
+    test('if a component has property "disabled" with a value of true, the ripple effect won\'t appear even if the component is clicked.', () => {
+        
+    })
 })
