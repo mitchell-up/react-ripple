6f506b875f978ff2947f1d86ff21a68b58d20448
mitchellkim94 <mitchellkim94@icloud.com>
2023-11-21 22:35:30 +0900

    test: ripple effect click

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,26 @@
 import { Config } from "jest";
 
 const config: Config = {
-    setupFiles: ['./__test__/setup.ts'],
+    setupFilesAfterEnv: ['./src/__test__/setup.ts'],
     transform: {
-        "^.+\\.(t|j)sx?$": "@swc/jest",
+        '^.+\\.(t|j)sx?$': [
+            '@swc/jest',
+            {
+                jsc: {
+                    transform: {
+                        react: {
+                            runtime: 'automatic',
+                        },
+                    },
+                },
+            },
+        ],
     },
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy'
+    },
+    testEnvironment: 'jsdom'
 }
 
 export default config
--- src/components/core/RippleEffect.test.tsx
+++ src/components/core/RippleEffect.test.tsx
@@ -1,22 +1,36 @@
-import { render } from '@testing-library/react'
+import { fireEvent, getByAltText, render, screen, waitFor } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import { RippleEffect } from './RippleEffect'
 import { ButtonHTMLAttributes } from 'react'
 
 const Tester: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
     return (
-        <button {...props}>
+        <button role='button' {...props}>
             <RippleEffect/>
         </button>
     )
 }
 
-const renderTester = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
-    return render(<Tester {...props}/>)
+const renderTester = (props?: ButtonHTMLAttributes<HTMLButtonElement>) => {
+    const { getByRole, ...others } = render(<Tester {...props}/>)
+
+    return {
+        ...others,
+        button: getByRole('button')
+    }
 }
 
 describe('Ripple', () => {
-    test('if a component has a RippleEffect inside itself and it is clicked, the ripple effect will appear.', () => {
-        
+    test('if a component has a RippleEffect inside itself and it is clicked, the ripple effect will appear.', async () => {
+        const { getByTestId, findByTestId } = renderTester()
+
+        const rippleContainer = getByTestId('ripple-container')
+
+        userEvent.click(rippleContainer)
+
+        const ripple = await findByTestId('ripple-effect')
+
+        expect(ripple).toBeInTheDocument()
     })
 
     test('if a component is wrapped by RippleEffectContainer and clicked, the ripple effect will appear.', () => {
